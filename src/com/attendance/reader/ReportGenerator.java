78da58269f195da2d7cd13c43f6cee01ecf118c1
Aryan214G <aryan214gm@gmail.com>
2025-08-20 11:44:48 +0530

    feat(ReportGenerator): enhance report generation with custom filename and extended employee data
    - Prompt user for report filename; fallback to default if empty
    - Append timestamp to report filename for uniqueness
    - Switched from ttendanceService.hoursWorked(sheet) to service.allEmployeesData()
    - Expanded CSV output to include 8 columns of employee data
    - Improved formatting of report output

--- src/com/attendance/reader/ReportGenerator.java
+++ src/com/attendance/reader/ReportGenerator.java
@@ -14,25 +14,33 @@ import org.apache.poi.ss.usermodel.Sheet;
 
 public class ReportGenerator {
 
-    public static void generateReport(Sheet sheet, AttendanceService service)) {
-        String fileNameSuffix = sc.nextLine().trim().replaceAll("\\s+", "_");
-        String fileName = "attendance_report_" + fileNameSuffix + ".csv";
+    public static void generateReport(Sheet sheet, AttendanceService service) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter file name for the report:");
+        String fileNameSuffix = sc.nextLine().trim(); 
+        if (fileNameSuffix.isEmpty())
+        {
+            fileNameSuffix = "default";
+        }   
+
+        String fileName = "attendance_report_" + fileNameSuffix + "_" 
+                  + System.currentTimeMillis() + ".csv";
+
         Path filePath = FileUtils.getReportPathFile(fileName);
         
         
-        Map<String, List<Double>> hoursWorked = attendanceService.hoursWorked(sheet);
+        Map<String, List<Double>> dataMap = service.allEmployeesData();
         
             try (BufferedWriter writer = Files.newBufferedWriter(filePath)) {
                 writer.write("Employee Name,Hours Worked,Hours Added,Total Hours Worked,Days Worked,Number of Working Days In The Month,Over Time (hours),Number of Single Punches\n");
-                for (Map.Entry<String, List<Double>> entry : hoursWorked.entrySet()) {
+                for (Map.Entry<String, List<Double>> entry : dataMap.entrySet()) {
                     String name = entry.getKey();
                     List<Double> data = entry.getValue();
-                    //  data: [workedHours, incompleteDays]
                     
                     
                     
-                    writer.write(String.format("%s,%.2f,%.2f,%.2f,%.0f\n",
-                        name, data.get(0), data.get(1), data.get(2), data.get(3)));
+                    writer.write(String.format("%s,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f\n",
+                        name, data.get(0), data.get(1), data.get(2), data.get(3), data.get(4), data.get(5), data.get(6)));
                 }
                 System.out.println("Report saved at: " + filePath.toString());
         } catch (IOException e) {
