f5c95291c259a46c640cf03bb582e0ea4ba1e6de
Aryan214G <aryan214gm@gmail.com>
2025-10-10 23:54:35 +0530

    Create ExcelLoaderView and update MainController
    - Added ExcelLoaderView.fxml
    - Updated MainController for scene switching
    - Noted error faced when running MainApp
    New commit message

--- src/main/java/com/attendance/ui/MainController.java
+++ src/main/java/com/attendance/ui/MainController.java
@@ -1,8 +1,38 @@
 package com.attendance.ui;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.MenuBar;
+import javafx.stage.Stage;
+
+import javafx.event.ActionEvent;
+import java.io.IOException;
 
 public class MainController {
 
+    @FXML
+    private MenuBar menuBar;
+    private Stage stage;
+    private Scene scene;
+
+    @FXML
+    public void switchToExcelLoaderScene(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/com/attendance/ui/ExcelLoaderView.fxml"));
+        stage = (Stage) menuBar.getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    public void switchToHomeScene(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/com/attendance/ui/MainView.fxml"));
+        scene = new Scene(root);
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
 
 }
