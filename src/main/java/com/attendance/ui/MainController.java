0517e146e2f30d412ed06d8d1670af8414e398cb
Aryan214G <aryan214gm@gmail.com>
2025-10-11 13:50:06 +0530

    Add new Form and Report views; update MainController and ExcelLoaderView

--- src/main/java/com/attendance/ui/MainController.java
+++ src/main/java/com/attendance/ui/MainController.java
@@ -38,5 +38,34 @@ public class MainController {
         stage.setScene(scene);
         stage.show();
     }
+    @FXML
+    public void switchToFormScene(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/com/attendance/ui/FormView.fxml"));
+        scene = new Scene(root);
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        String css = this.getClass().getResource("/com/attendance/ui/styles.css").toExternalForm();
+        scene.getStylesheets().add(css);
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    public void switchToReportScene(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/com/attendance/ui/ReportView.fxml"));
+        scene = new Scene(root);
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        String css = this.getClass().getResource("/com/attendance/ui/styles.css").toExternalForm();
+        scene.getStylesheets().add(css);
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    public void switchToExcelLoaderSceneFromFormScene(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/com/attendance/ui/ExcelLoaderView.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow(); // <— use this line
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
 
 }
